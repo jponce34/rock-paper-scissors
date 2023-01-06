79c4531537d9d57c2fcf8356d09af90f360b6921
Jonathan Ponce <jponce34@gmail.com>
2023-01-05 23:52:03 -0800

    as

--- rps.js
+++ rps.js
@@ -3,71 +3,84 @@ let playerscore = 0; // set playerscore to zero
 let computerscore = 0; //set computerscore to zero
 let computerSelection; //declare an empty computerselection variable
 let winner = ""; // declare an blank winner
-const choice = ["rock", "paper", "scissor"]//an array for choices
-
+const computer = ["rock", "paper", "scissor"]//an array for choices
+let roundmax = 5;
 const allchoices = document.querySelectorAll('button');
 const playerChoiceDisplay = document.getElementById('player-choice');
-const computerchoiceDisplay = document.getElementsByID('computer-choice');
-const resultDisplay = document.getElementsByID('results');
+const computerchoiceDisplay = document.getElementById('computer-choice');
+const resultDisplay = document.getElementById('results');
+const resultDisplay2 = document.getElementById('results2');
 
-allchoices.forEach(choice => choice.addEventListener('click', (e) =>{
-    playerSelection = e.target.id;
-    playerChoiceDisplay.innerHTML = playerSelection;
-    getComputerChoice();
-    playRound();
-}))
+    allchoices.forEach(allchoices => {
+        allchoices.addEventListener('click', (e) => {
+            playerSelection = e.target.id;
+            playerChoiceDisplay.innerHTML = "Player selects " + playerSelection;
+            getComputerChoice();
+            playRound(playerSelection, computerSelection);
+            if (playerscore >= roundmax || computerscore >= roundmax){
+                gameResult();
+                return alert("Game over");
+            }
+        })
+    })
 
 //function for getting computer choice
 function getComputerChoice(){
-    return choice[Math.floor(Math.random()*3)];//should return a random value between 0-2
+   computerSelection = computer[Math.floor(Math.random()*3)];//should return a random value between 0-2
+    computerchoiceDisplay.innerHTML = "computer selects " + computerSelection;
 }
 
 //function for one round of the game
-function playRound(playerSelection,computerSelection,round){
+function playRound(playerSelection,computerSelection){
     //if and else if statments for scenarios
     if(playerSelection === "rock" && computerSelection === "paper"){
         computerscore += 1; //increment computer score by one
-        console.log("You lose " + computerSelection +" beats " + playerSelection);
+        resultDisplay.innerHTML  = "Player score " + playerscore;
+        resultDisplay2.innerHTML = "computer score " + computerscore;
+        
     }
     else if(playerSelection === "rock" && computerSelection === "scissor"){
         playerscore += 1; //increment player score by one
-        console.log("You win "+ playerSelection + " beats " + computerSelection);
+        resultDisplay.innerHTML  = "player score " + playerscore;
+        resultDisplay2.innerHTML  = "computer score " + computerscore;
     }
     else if(playerSelection === "scissors" && computerSelection === "paper"){
         playerscore += 1; //increment computer score by one
-        console.log("You win "+ playerSelection + " beats " + computerSelection);
+        resultDisplay.innerHTML  = "Player score " + playerscore;
+        resultDisplay2.innerHTML  = "computer score " + computerscore;
     }
     else if(playerSelection === "scissors" && computerSelection === "rock"){
         computerscore += 1; //increment computer score by one
-        console.log("You lose " + computerSelection +" beats " + playerSelection);
+        resultDisplay.innerHTML  = "Player score " + playerscore;
+        resultDisplay2.innerHTML  = "computer score " + computerscore;
+        
     }
     else if(playerSelection === "paper" && computerSelection === "rock"){
         playerscore += 1; //increment computer score by one
-        console.log("You win "+ playerSelection + " beats " + computerSelection);
+        resultDisplay.innerHTML  = "Player score " + playerscore;
+        resultDisplay2.innerHTML  = "computer score " + computerscore;
     }
     else if (playerSelection === "paper" && computerSelection === "scissor"){
         computerscore += 1; //increment computer score by one
-        console.log("You lose " + computerSelection +" beats " + playerSelection);
-    }
-    else {
-        console.log("draw"); // draw if both selections are the same
+        resultDisplay.innerHTML  = "Player score " + playerscore;
+        resultDisplay2.innerHTML  = "computer score " + computerscore;
+        
     }
 }
 
 //function for game with return value of winner
-function game(){
-    let round = 1;
+function gameResult(){
     // declare and set round variable to 1 so player knows what round it is
-    if (playerscore > computerscore){
-        winner = "player"; // sets winner to player
-    }
-    else if (playerscore < computerscore ){
-        winner = "computer"; // sets winner to computer
-    }
-    else {
-        winner = "draw" // sets winner to draw if score is even
-    }
-    console.log("playerscore: " + playerscore); // prints out player score
-    console.log("computerscore: " + computerscore); // prints out computer score
-    return winner; // returns the winner from the function
+        if (playerscore > computerscore){
+            winner = "player"; // sets winner to player
+            console.log("Player wins")
+        }
+        else if (playerscore < computerscore ){
+            winner = "computer"; // sets winner to computer
+            console.log("computer wins");
+        }
+        else {
+            winner = "draw" // sets winner to draw if score is even
+            console.log("Game is a tie");
+        }
 }
